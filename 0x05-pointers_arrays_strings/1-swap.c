96d4be86387f34869ee765926b730a201b6c54aa
DoveRain21 <eagleklowd@gmail.com>
2023-02-28 00:30:26 -0800

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * swap_int - main function
  * @a: first integer
  * @b: second integer
- *
  * Description: function that swaps the values of two integers.
  *
  * Return: none
  */
 void swap_int(int *a, int *b)
-{
-	int d;
 
-	d = *a;
+{
+	int m;
+
+	m = *a;
 	*a = *b;
-	*b = d;
+	*b = m;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,24 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 
 #endif
-
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
