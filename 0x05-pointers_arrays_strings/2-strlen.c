ba41781da0074ad72bc2ef71989932ee34f51fc2
DoveRain21 <eagleklowd@gmail.com>
2023-02-27 18:11:49 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - main function
+ * @s: Pointer of a char address
+ *
+ * Description: a function that returns the length of a string
+ *
+ * Return: none
+ */
+int _strlen(char *s);
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
