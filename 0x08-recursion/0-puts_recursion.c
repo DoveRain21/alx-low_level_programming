d6f2b5a5ad85e6527782dff43ebf559f44a95607
DoveRain21 <eagleklowd@gmail.com>
2023-03-30 12:03:09 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
-* _puts_recursion - prints a string followed by a new line
-*@s:character to be printed
-*Return:returns void
-*/
+ * _puts_recursion - prints a string followed by a new line
+ *@s: character to be printed
+ *Return: returns void
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
